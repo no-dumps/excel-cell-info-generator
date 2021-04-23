9440bbbc2d618c5ed51abadd9a7a16043b630397
n-oouchi <noouchi4@gmail.com>
2021-04-24 01:30:42 +0900

    :heavy_plus_sign: セル情報モデル作成

--- src/main/java/nodump/noouchi/cellinfogenerator/domain/ExcelCellInfo.java
+++ src/main/java/nodump/noouchi/cellinfogenerator/domain/ExcelCellInfo.java
@@ -1,5 +1,25 @@
 package nodump.noouchi.cellinfogenerator.domain;
 
 public class ExcelCellInfo {
+    public String cellRange;
     public String value;
+    public String alignment;
+    public String borderBottom;
+    public String borderLeft;
+    public String borderRight;
+    public String borderTop;
+    public String bottomBorderColor;
+    public String leftBorderColor;
+    public String rightBorderColor;
+    public String topBorderColor;
+    public String dataFormat;
+    public String fillBackgroundColor;
+    public String fillForegroundColor;
+    public String fillPattern;
+    public String font;
+    public String hidden;
+    public String indention;
+    public String locked;
+    public String rotation;
+    public String verticalAlignment;
 }
