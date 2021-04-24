762f8200037357aeb4d2bfcc48f91c27146ad152
n-oouchi <noouchi4@gmail.com>
2021-04-24 20:28:28 +0900

    :sparkles: セル情報をjsonで出力

--- src/main/java/nodump/noouchi/cellinfogenerator/domain/ExcelCellInfo.java
+++ src/main/java/nodump/noouchi/cellinfogenerator/domain/ExcelCellInfo.java
@@ -1,5 +1,11 @@
 package nodump.noouchi.cellinfogenerator.domain;
 
+import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+
+import java.util.Arrays;
+
 public class ExcelCellInfo {
     public String cellRange;
     public String value;
@@ -16,10 +22,86 @@ public class ExcelCellInfo {
     public String fillBackgroundColor;
     public String fillForegroundColor;
     public String fillPattern;
-    public String font;
+    public Font font;
     public String hidden;
     public String indention;
     public String locked;
     public String rotation;
     public String verticalAlignment;
+
+    public ExcelCellInfo(XSSFCell cell) {
+        cellRange = cell.getReference();
+        value = getCellValueToString(cell);
+        alignment = cell.getCellStyle().getAlignment().name();
+        borderBottom = cell.getCellStyle().getBorderBottom().name();
+        borderLeft = cell.getCellStyle().getBorderLeft().name();
+        borderRight = cell.getCellStyle().getBorderRight().name();
+        borderTop = cell.getCellStyle().getBorderTop().name();
+        bottomBorderColor = String.valueOf(cell.getCellStyle().getBottomBorderColor());
+        leftBorderColor = String.valueOf(cell.getCellStyle().getLeftBorderColor());
+        rightBorderColor = String.valueOf(cell.getCellStyle().getRightBorderColor());
+        topBorderColor = String.valueOf(cell.getCellStyle().getTopBorderColor());
+        dataFormat = cell.getCellStyle().getDataFormatString();
+        fillBackgroundColor = String.valueOf(cell.getCellStyle().getFillBackgroundColorColor());
+        fillForegroundColor = String.valueOf(cell.getCellStyle().getFillForegroundColorColor());
+        fillPattern = cell.getCellStyle().getFillPattern().name();
+        font = new Font(cell.getCellStyle().getFont());
+        hidden = String.valueOf(cell.getCellStyle().getHidden());
+        indention = String.valueOf(cell.getCellStyle().getIndention());
+        locked = String.valueOf(cell.getCellStyle().getLocked());
+        rotation = String.valueOf(cell.getCellStyle().getRotation());
+        verticalAlignment = cell.getCellStyle().getVerticalAlignment().name();
+    }
+
+    private static String getCellValueToString(XSSFCell cell) {
+        switch (cell.getCellType()) {
+            case STRING:
+                return cell.getStringCellValue();
+            case NUMERIC:
+                if (DateUtil.isCellDateFormatted(cell)) {
+                    return String.valueOf(cell.getDateCellValue());
+                } else {
+                    return String.valueOf(cell.getNumericCellValue());
+                }
+            case BOOLEAN:
+                return String.valueOf(cell.getBooleanCellValue());
+            case FORMULA:
+                return String.valueOf(cell.getCellFormula());
+            case ERROR:
+                return String.valueOf(cell.getErrorCellValue());
+            case BLANK:
+            case _NONE:
+                return "";
+            default:
+                throw new IllegalStateException("Unexpected value: " + cell.getCellType());
+        }
+    }
+
+    private static class Font {
+        public String bold;
+        public String charSet;
+        public String xssfColor;
+        public String themeColor;
+        public String fontHeight;
+        public String fontHeightInPoints;
+        public String fontName;
+        public String italic;
+        public String strikeout;
+        public String typeOffset;
+        public String underline;
+
+        public Font(XSSFFont font) {
+            bold = String.valueOf(font.getBold());
+            charSet = String.valueOf(font.getCharSet());
+            xssfColor = font.getXSSFColor() == null ? null : Arrays.toString(font.getXSSFColor().getRGB());
+            themeColor = String.valueOf(font.getThemeColor());
+            fontHeight = String.valueOf(font.getFontHeight());
+            fontHeightInPoints = String.valueOf(font.getFontHeightInPoints());
+            fontName = String.valueOf(font.getFontName());
+            italic = String.valueOf(font.getItalic());
+            strikeout = String.valueOf(font.getStrikeout());
+            typeOffset = String.valueOf(font.getTypeOffset());
+            underline = String.valueOf(font.getUnderline());
+        }
+    }
 }
--- src/main/java/nodump/noouchi/cellinfogenerator/domain/ExcelSheetService.java
+++ src/main/java/nodump/noouchi/cellinfogenerator/domain/ExcelSheetService.java
@@ -3,17 +3,17 @@ package nodump.noouchi.cellinfogenerator.domain;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.DateUtil;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 
 public class ExcelSheetService {
     public static String getAllCellInfo(XSSFSheet sheet) throws JsonProcessingException {
-        ExcelCellInfo excelCellInfo = new ExcelCellInfo();
+        ArrayList<ExcelCellInfo> excelCellInfoList = new ArrayList<>();
 
         Iterator<Row> rowIterator = sheet.rowIterator();
         while (rowIterator.hasNext()) {
@@ -21,36 +21,12 @@ public class ExcelSheetService {
             Iterator<Cell> cellIterator = row.cellIterator();
             while (cellIterator.hasNext()) {
                 XSSFCell cell = (XSSFCell) cellIterator.next();
-                System.out.println(getCellValueToString(cell));
+                ExcelCellInfo excelCellInfo = new ExcelCellInfo(cell);
+                excelCellInfoList.add(excelCellInfo);
             }
         }
-
         ObjectMapper mapper = new ObjectMapper();
-        String json = mapper.writeValueAsString(excelCellInfo);
+        String json = mapper.writeValueAsString(excelCellInfoList);
         return json;
-    }
-
-    public static String getCellValueToString(XSSFCell cell) {
-        switch (cell.getCellType()) {
-            case STRING:
-                return cell.getStringCellValue();
-            case NUMERIC:
-                if (DateUtil.isCellDateFormatted(cell)) {
-                    return String.valueOf(cell.getDateCellValue());
-                } else {
-                    return String.valueOf(cell.getNumericCellValue());
-                }
-            case BOOLEAN:
-                return String.valueOf(cell.getBooleanCellValue());
-            case FORMULA:
-                return String.valueOf(cell.getCellFormula());
-            case ERROR:
-                return String.valueOf(cell.getErrorCellValue());
-            case BLANK:
-            case _NONE:
-                return "";
-            default:
-                throw new IllegalStateException("Unexpected value: " + cell.getCellType());
-        }
     }
 }
